ca82a6ec1fb51ebeb0c8dcceb0bcd1190ecff03e
helen <he.mora@duocuc.cl>
2024-12-01 18:50:48 -0300

    cambios 2
    agregue el mapa en ambos perfiles (pasajero y conductor)

--- conejillodeindias2/src/app/profile/profile.page.ts
+++ conejillodeindias2/src/app/profile/profile.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import * as mapboxgl from 'mapbox-gl';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-profile',
@@ -8,12 +10,42 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProfilePage implements OnInit {
   username: string = '';
+  map!: mapboxgl.Map;
 
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {
-    this.route.queryParams.subscribe((params) => {
+  async ngOnInit() {
+    this.route.queryParams.subscribe((params: { [key: string]: string }) => {
       this.username = params['username'] || 'Usuario';
     });
+
+    // Configurar el token de acceso de Mapbox
+    (mapboxgl as any).accessToken = 'pk.eyJ1IjoidmEtbGF0b3JyZSIsImEiOiJjbTN4OHM2dWMxZzZ6Mmpvcm9mMXdwb3JzIn0.NM_QPXtsWhgSvRZ_NnKRxQ';
+
+    // Inicializar el mapa
+    this.map = new mapboxgl.Map({
+      container: 'map', 
+      style: 'mapbox://styles/mapbox/streets-v11', // Estilo del mapa
+      center: [-74.006, 40.7128], // Centro inicial del mapa [lng, lat]
+      zoom: 12, // Nivel de zoom inicial
+    });
+
+    // Obtener la ubicación actual y agregar un marcador
+    const coordinates = await this.getCurrentPosition();
+    if (coordinates) {
+      const { latitude, longitude } = coordinates;
+      this.map.setCenter([longitude, latitude]);
+      new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(this.map);
+    }
+  }
+
+  async getCurrentPosition() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      return position.coords;
+    } catch (error) {
+      console.error('Error obteniendo la ubicación:', error);
+      return null;
+    }
   }
 }
