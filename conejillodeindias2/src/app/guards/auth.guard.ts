75b71965c5d3e7bcb218185adef95de4c0ded38f
Bale <va.latorre@duocuc.cl>
2024-12-13 22:41:49 -0300

    a

--- conejillodeindias2/src/app/guards/auth.guard.ts
+++ conejillodeindias2/src/app/guards/auth.guard.ts
@@ -1,5 +1,16 @@
 import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../Services/authenticator.service';
+import { Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (!authService.isConnected()) {
+    router.navigate(['/home']);
+    return false;
+  }
+
   return true;
 };
